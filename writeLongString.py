c5f83311196f5395ca1247d216300509096b6e3d
LAPTOP\hagen <hagen.sal@gmail.com>
2019-01-16 19:47:14 +0100

    small edits and started tfidf

--- writeLongString.py
+++ writeLongString.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pickle as p
 import re
 
 li_years = [1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018]
@@ -54,5 +55,5 @@ def writeLongString(df, querystring='', domain='undefined', text_col='tekst', da
 	print('Done! Written text file: data/longstrings/' + filename + '.txt')
 
 if __name__ == '__main__':
-	df = pd.read_csv('data/media/kranten/all-islam-moslim-moslims-atleast5.csv')
-	writeLongString(df, querystring='', domain='kranten',text_col='full_text')
+	df = pd.read_csv('data/politiek/handelingen/all-handelingen.csv')
+	writeLongString(df, querystring='multiculturele', domain='politiek',text_col='tekst')
